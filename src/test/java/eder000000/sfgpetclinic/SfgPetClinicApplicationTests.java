35304a45accb8385c4c73f432d92dfc0573ff426
edera <edera@amdocs.com>
2022-09-15 01:34:14 -0500

    First commit

---
+++ src/test/java/eder000000/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package eder000000.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
